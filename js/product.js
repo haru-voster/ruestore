b79334270fdd814fe5849e2881585d1c78e2eeef
haru-voster <wabwireharoun2001@gmail.com>
2026-02-16 15:11:00 +0300

    removal constant

--- js/product.js
+++ js/product.js
@@ -7,18 +7,7 @@ const categories = {
   5:"Cameras"
 };
 
-const products = [
-{id:101,name:"HP EliteBook 840",price:98000,oldPrice:105000,categoryId:1,image:"./img/product01.png",description:"Core i7,16GB RAM,512 SSD"},
-{id:102,name:"Dell Optiplex",price:72000,oldPrice:80000,categoryId:2,image:"./img/product02.png",description:"Core i5 Desktop Set"},
-{id:103,name:"Gaming Headset",price:4500,oldPrice:6000,categoryId:4,image:"./img/product03.png",description:"RGB Noise Canceling"},
-{id:104,name:"Canon Camera",price:55000,oldPrice:65000,categoryId:5,image:"./img/product04.png",description:"24MP HD Camera"},
-{id:105,name:"Samsung Galaxy S22",price:85000,oldPrice:95000,categoryId:3,image:"./img/product05.png",description:"128GB Storage"},
-{id:101,name:"HP EliteBook 840",price:98000,oldPrice:105000,categoryId:1,image:"./img/product01.png",description:"Core i7,16GB RAM,512 SSD"},
-{id:102,name:"Dell Optiplex",price:72000,oldPrice:80000,categoryId:2,image:"./img/product02.png",description:"Core i5 Desktop Set"},
-{id:103,name:"Gaming Headset",price:4500,oldPrice:6000,categoryId:4,image:"./img/product03.png",description:"RGB Noise Canceling"},
-{id:104,name:"Canon Camera",price:55000,oldPrice:65000,categoryId:5,image:"./img/product04.png",description:"24MP HD Camera"},
-{id:105,name:"Samsung Galaxy S22",price:85000,oldPrice:95000,categoryId:3,image:"./img/product05.png",description:"128GB Storage"}
-];
+
 
 function loadProducts(category="all"){
 const container=document.getElementById("productContainer");
