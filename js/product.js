f972873874ba550db8c1c553a2a866ad38ed3eca
haru-voster <wabwireharoun2001@gmail.com>
2026-02-16 13:45:22 +0300

    js

---
+++ js/product.js
@@ -0,0 +1,36 @@
+const categories = [
+  { id: 1, name: "Laptops" },
+  { id: 2, name: "Desktops" },
+  { id: 3, name: "Accessories" },
+  { id: 4, name: "Cameras" }
+];
+
+const products = [
+  {
+    id: 101,
+    name: "HP EliteBook 840",
+    price: 98000,
+    oldPrice: 105000,
+    categoryId: 1,
+    image: "./img/product01.png",
+    description: "Core i7, 16GB RAM, 512 SSD"
+  },
+  {
+    id: 102,
+    name: "Dell Optiplex",
+    price: 72000,
+    oldPrice: 80000,
+    categoryId: 2,
+    image: "./img/product02.png",
+    description: "Core i5 Desktop Set"
+  },
+  {
+    id: 103,
+    name: "Gaming Headset",
+    price: 4500,
+    oldPrice: 6000,
+    categoryId: 3,
+    image: "./img/product03.png",
+    description: "RGB Noise Canceling"
+  }
+];
